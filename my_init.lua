b5721fd357407f0115dcd8ebdd1c2029af206d03
Juhani Kataja <juhani.kataja@csc.fi>
2025-02-21 10:19:33 +0200

    added csvview

--- my_init.lua
+++ my_init.lua
@@ -52,12 +52,11 @@ require("lazy").setup({
         vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
         vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
     end,
-    {"olical/conjure",
-    ft = { "clojure", "fennel", "python", "julia"}, -- etc
-    lazy = true,
-  },
-  "voldikss/vim-floaterm",
 },
+{"olical/conjure",
+     ft = { "clojure", "fennel", "python", "julia"}, -- etc
+     lazy = true, },
+  "voldikss/vim-floaterm",
 
 -- { -- MarkdownPreview
 --     "iamcco/markdown-preview.nvim",
@@ -68,12 +67,37 @@ require("lazy").setup({
 
 --  "SirVer/ultisnips",
 --  "quangnguyen30192/cmp-nvim-ultisnips"
+
+{
+  "hat0uma/csvview.nvim",
+  ---@module "csvview"
+  ---@type CsvView.Options
+  opts = {
+    parser = { comments = { "#", "//" } },
+    keymaps = {
+      -- Text objects for selecting fields
+      textobject_field_inner = { "if", mode = { "o", "x" } },
+      textobject_field_outer = { "af", mode = { "o", "x" } },
+      -- Excel-like navigation:
+      -- Use <Tab> and <S-Tab> to move horizontally between fields.
+      -- Use <Enter> and <S-Enter> to move vertically between rows and place the cursor at the end of the field.
+      -- Note: In terminals, you may need to enable CSI-u mode to use <S-Tab> and <S-Enter>.
+      jump_next_field_end = { "<Tab>", mode = { "n", "v" } },
+      jump_prev_field_end = { "<S-Tab>", mode = { "n", "v" } },
+      jump_next_row = { "<Enter>", mode = { "n", "v" } },
+      jump_prev_row = { "<S-Enter>", mode = { "n", "v" } },
+    },
+  },
+  cmd = { "CsvViewEnable", "CsvViewDisable", "CsvViewToggle" },
+},
+
 })
 
 
 --require("lspconfig").lua_ls.setup{}
 require("lspconfig").fortls.setup{}
 require("lspconfig").clangd.setup{}
+-- require('lspconfig').julials.setup{}
 
 --- fzf
 ---require("fzf").setup()
@@ -258,3 +282,5 @@ require('peek').setup({
     capabilities = capabilities
   }
 
+-- conjure things
+-- vim.g["conjure#mapping#doc_word"] = false
